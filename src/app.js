a0a8ec052bb9287e2a62a40ecf8d0ecaf3e160fc
OTELLO BOZ MARALDO <oboz@agn.gov.ar>
2024-03-01 16:55:28 -0300

    Server Express

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import ProductManager from './productManager.js';
+
+const myProducts = new ProductManager();
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.get('/products', async (req, res) => {
+    try{
+        const products = await myProducts.getProducts();
+        res.json(products);
+    }catch(error){
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+
+const PORT = 8080;
+
+app.listen(PORT, () => console.log("Server Started"));
---
+++ src/productManager.js
@@ -0,0 +1,99 @@
+import { writeFileSync, promises, readFileSync } from 'fs';
+
+//Genero clase constructora ProductManager
+class ProductManager {
+    //Creo un array vacio y agrego directorio a usar mi products.json.
+	constructor() {
+        this.path = '../myFile';
+    };
+
+    //Función Agregar productos
+    addProducts = (myProduct) => { 
+
+        const { title, description, price, thumbnail, code, stock } = myProduct;
+        
+        //Chequeo que los campos sean obligatorios y que "code" no se repita, tirando el mensaje correspondiente.
+        const duplicatedCode = this.products.some((product) => product.code === code);
+        
+        if (title && description && price && thumbnail && code && stock) {
+
+            //Si el numero de codigo no se duplica escribir archivo con el producto.
+            if (!duplicatedCode) {
+                let id = this.products.length + 1;
+                this.products.push({id, title, description, price, thumbnail, code, stock});
+                writeFileSync(`${this.path}/products.json`, JSON.stringify(this.products, null, '\t'));
+                return this.products;
+            }else{
+                console.log("El código no puede estar repetido");
+            }
+        }else{
+            console.log("Todos los campos son obligatorios");
+        }
+    };
+
+    //Leo el archivo y veo si realmente existe.
+    getProducts = async () => {
+            try {
+                let myFile = await promises.readFile(`${this.path}/products.json`, 'utf8');
+                return JSON.parse(myFile);
+            } catch (error) {
+                console.error(error);
+                throw error;
+            }
+        }
+
+    //Quiero ver si el id existe usando como parametro un id que yo le asigno al método.
+    getProductById = async (myId) => {
+        await promises.readFile(`${this.path}/products.json`, 'utf8');
+        const myProduct = this.products.find((product) => product.id === myId);
+        if (myProduct) {
+            return myProduct;
+        }else{
+            console.log(`Product with ID ${myId} Not Found`);
+        }
+    }
+
+    updateProduct = (myId, myProduct) => {
+        try {
+            const index = this.products.findIndex((product) => product.id === myId);
+            if (index >= 0) {
+                
+                // Si el ID del Producto es diferente al que yo le quiero asignar muestra NOT FOUND.
+                if (myProduct.id && myProduct.id !== myId) {
+                    console.log("Updating ID is forbidden!!! >("); //Prohibo el uso de modificar el ID con el IF de arriba.
+                    return;
+                } else {
+                    this.products[index] = { ...this.products[index], ...myProduct, id: myId };
+                    writeFileSync(`${this.path}/products.json`, JSON.stringify(this.products, null, '\t'));
+                }
+            } else {
+                console.log(`Product with ID ${myId} Not Found`);
+            }
+        } catch(error) {
+            console.error(error);
+        }
+    }
+
+    deleteProduct = (myId) => {
+        try {
+            let myFile = readFileSync(`${this.path}/products.json`, 'utf8');
+            this.products = JSON.parse(myFile);
+
+            //Busco el producto a borrar con ese mismo ID.
+            const deleteProduct = this.products.findIndex((product) => product.id === myId);
+
+            if(deleteProduct >= 0) {
+                //Borro el producto con Splice.
+                this.products.splice(deleteProduct, 1);
+                writeFileSync(`${this.path}/products.json`, JSON.stringify(this.products, null, '\t'));
+                console.log(`Product with ${myId} deleted.`);
+            }else{
+                console.log(`Product with ${myId} does not exist.`)
+            }
+        }catch (error) {
+            console.error(error);
+        }
+    }
+}
+
+export default ProductManager;
