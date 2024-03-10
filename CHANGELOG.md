78ae24ad5802955facc3cafaa91bc390d55eb669
OTELLO BOZ MARALDO <oboz@agn.gov.ar>
2024-03-10 20:06:15 -0300

    Cart Check

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,13 @@
 
 Desarrollar el servidor basado en Node.JS y express, que escuche en el puerto 8080 y disponga de dos grupos de rutas: /products y /carts. Dichos endpoints estarán implementados con el router de express.
 
+## 10-03-2024
+
+Fixed to Empty Cart Array in POST /.
+Fixed Cart and Product ID check.
+Fixed Quantity added with id of that Product to that Cart.
+For now the product will create only if cart exists.
+
 ## 09-03-2024
 
 Added Cart Array.
--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -7,7 +7,7 @@ const router = Router();
 
 let carts = [];
 const path = myProducts.path;
-let quantity = 0;
+let quantity = 1;
 
 if (!existsSync(`${path}/carts.json`)) {
     writeFileSync(`${path}/carts.json`, JSON.stringify(carts, null, '\t'));
@@ -18,28 +18,21 @@ router.post("/", async (req, res) => {
         let myFile;
         let { products } = req.body;
 
-        if (!Array.isArray(products)) {
-            products = [products];
-        }
+        products = [];
+        
+        myFile = await promises.readFile(`${path}/carts.json`, 'utf8');
+        carts = JSON.parse(myFile);
+        
+        let id = carts.length + 1;
+        carts.push({ id, products });
+        await promises.writeFile(`${path}/carts.json`, JSON.stringify(carts, null, '\t'));
 
-        const product = products.map((value, key) => ({ [key]: value }));
-
-        if (products.length > 0) {
-            myFile = await promises.readFile(`${path}/carts.json`, 'utf8');
-            carts = JSON.parse(myFile);
-            
-            let id = carts.length + 1;
-            carts.push({id, products: product});
-            await promises.writeFile(`${path}/carts.json`, JSON.stringify(carts, null, '\t'));
-
-            return res.status(201).send({ message: "Product successfully added to cart!" });
-        }else{
-            return res.status(400).send({ message: "No products to add to cart!" });
-        }
+        return res.status(201).send({ message: "Product successfully created in cart!" });
     } catch (error) {
         return res.status(500).send('Could not add product to cart.');
     }
 });
+
 
 router.get('/:cid', async (req, res) => {
     let cid = parseInt(req.params.cid);
@@ -51,44 +44,41 @@ router.get('/:cid', async (req, res) => {
     if (cart) { 
         return res.status(200).send(cart) 
     }else{
-        return res.status(400).send({ message: "No products found with this ID" });
+        return res.status(400).send({ message: "No cart found with this ID" });
     }
 });
 
 router.post("/:cid/product/:pid", async (req, res) => {
-    const product = parseInt(req.params.pid);
-    
-    let myProductsFile = await promises.readFile(`${path}/products.json`, 'utf8');
-    const myProducts = JSON.parse(myProductsFile);
-    
-    const myIdChecker = myProducts.some((item) => item.id === product);
+    const cartId = parseInt(req.params.cid);
+    const productId = parseInt(req.params.pid);
 
-    if(myIdChecker) {
+    try {
         let myFile = await promises.readFile(`${path}/carts.json`, 'utf8');
         const myCarts = JSON.parse(myFile);
 
-        if (myCarts.length !== 0) {
-            myCarts.forEach(element => {
-                const products = element.products;
-                const sameProduct = myProducts.find((item) => item.id === product);
-                if(sameProduct) {
-                    const myProd = myCarts.find(item => item.id === sameProduct.id);
-                    if(myProd) {
-                        quantity = quantity+1;
-                    }
-                }else{
-                    products.push({product: product, quantity: quantity});
-                }
-            });
+        let myProductsFile = await promises.readFile(`${path}/products.json`, 'utf8');
+        const myProducts = JSON.parse(myProductsFile);
+        
+        const myCart = myCarts.find(cart => cart.id === cartId);
+        const myProduct = myProducts.find(product => product.id === productId);
+        
+        if (myCart && myProduct) {
+            const productCheck = myCart.products.find(item => item.product === productId);
+
+            if (productCheck) {
+                productCheck.quantity++;
+            } else {
+                myCart.products.push({ product: productId, quantity: 1 });
+            }
+            
+            await promises.writeFile(`${path}/carts.json`, JSON.stringify(myCarts, null, '\t'));
+
+            return res.status(201).send({ message: "Product successfully added to cart!" });
         }else{
-            myCarts.push({ id: 1, products: [{ product: product, quantity: quantity }] });
+            return res.status(400).send({ message: "Could not find product or cart with this ID" });
         }
-
-        await promises.writeFile(`${path}/carts.json`, JSON.stringify(myCarts, null, '\t'));
-
-        return res.status(201).send({ message: "Product successfully added to cart!" });
-    }else{
-        return res.status(400).send({ message: "Could not find product with this ID" });
+    } catch (error) {
+        return res.status(500).send('Could not add product to cart.');
     }
 });
 
