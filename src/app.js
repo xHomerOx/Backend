3b5d5e64c4484398055dd410d01c2a4362911554
OTELLO BOZ MARALDO <oboz@agn.gov.ar>
2024-03-17 18:38:49 -0300

    Handlebars

--- src/app.js
+++ src/app.js
@@ -1,24 +1,17 @@
 import express from 'express';
 import productsRouter from './routes/products.router.js'
 import cartsRouter from './routes/carts.router.js'
+import handlebars from 'express-handlebars';
+import __dirname from './utils.js';
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.engine('handlebars', handlebars.engine());
 
-app.use("/api/products", productsRouter);
-app.use("/api/carts", cartsRouter);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'handlebars');
 
-async function myGreetings() {
-    return '<font face="arial">Ir a "/products" para ver el listado</font>'
-}
-
-app.get('/', async (_req, res) => {
-    let greetings = await myGreetings();
-    res.send(greetings);
-})
+app.use(express.static(__dirname + '/public'))
 
 const PORT = 8080;
 
