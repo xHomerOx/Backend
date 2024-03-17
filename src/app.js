95eaf052cdb6bfd91c3373693292e622240a70e9
OTELLO BOZ MARALDO <oboz@agn.gov.ar>
2024-03-17 19:35:33 -0300

    Handlebars

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
 import express from 'express';
-import handlebars from 'express-handlebars';
+import exphbs from 'express-handlebars';
+import path from 'path';
 import __dirname from './utils.js';
+import router from './routes/carts.router.js';
 
 const app = express();
 
-app.engine('handlebars', handlebars.engine());
+//Para que solo renderice la vista Home y no el Layout por defecto de Handlebars.
+app.engine('handlebars', exphbs.engine({ defaultLayout: false }));
 
-app.set('views', __dirname + '/views');
+//Configuro motor y estructura del Proyecto.
 app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(__dirname + '/public'))
+
+app.use(express.static(__dirname + '/public'));
+app.use("/", router);
 
 const PORT = 8080;
 
--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -115,68 +115,12 @@ const products = [
     }
 ];
 
-quantity = 1;
-
-// router.post("/", async (req, res) => {
-//     try {
-//         let myFile;
-//         let { products } = req.body;
-
-//         let id = carts.length + 1;
-//         carts.push({ id, products });
-//         await promises.writeFile(`${path}/carts.json`, JSON.stringify(carts, null, '\t'));
-
-//         return res.status(201).send({ message: "Product successfully created in cart!" });
-//     } catch (error) {
-//         return res.status(500).send('Could not add product to cart.');
-//     }
-// });
-
 
 router.get('/', async (req, res) => {
-    let pid = parseInt(req.params.cid);
-
-    const cart = products.find((cart) => cart.id === pid);
-
-    if (cart) { 
-        return res.status(200).send(cart) 
-    }else{
-        return res.status(400).send({ message: "No cart found with this ID" });
-    }
+    res.render(
+        "home",
+        { products }
+    )
 });
-
-// router.post("/:cid/product/:pid", async (req, res) => {
-//     const cartId = parseInt(req.params.cid);
-//     const productId = parseInt(req.params.pid);
-
-//     try {
-//         let myFile = await promises.readFile(`${path}/carts.json`, 'utf8');
-//         const myCarts = JSON.parse(myFile);
-
-//         let myProductsFile = await promises.readFile(`${path}/products.json`, 'utf8');
-//         const myProducts = JSON.parse(myProductsFile);
-        
-//         const myCart = myCarts.find(cart => cart.id === cartId);
-//         const myProduct = myProducts.find(product => product.id === productId);
-        
-//         if (myCart && myProduct) {
-//             const productCheck = myCart.products.find(item => item.product === productId);
-
-//             if (productCheck) {
-//                 productCheck.quantity++;
-//             } else {
-//                 myCart.products.push({ product: productId, quantity: 1 });
-//             }
-            
-//             await promises.writeFile(`${path}/carts.json`, JSON.stringify(myCarts, null, '\t'));
-
-//             return res.status(201).send({ message: "Product successfully added to cart!" });
-//         }else{
-//             return res.status(400).send({ message: "Could not find product or cart with this ID" });
-//         }
-//     } catch (error) {
-//         return res.status(500).send('Could not add product to cart.');
-//     }
-// });
 
 export default router;
