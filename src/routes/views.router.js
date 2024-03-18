02065456a0a4483dfb11539faa059e1938eeafc0
OTELLO BOZ MARALDO <oboz@agn.gov.ar>
2024-03-18 10:24:30 -0300

    Websocket

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -3,11 +3,21 @@ import __dirname from '../utils.js';
 
 //Me traigo solo el Objeto Products.
 import { products } from './carts.router.js';
+import ProductManager from '../productManager.js';
+
+const myProducts = new ProductManager();
 
 const viewsRouter = Router();
 
 viewsRouter.get('/', (_req, res) => {
   res.render('realTimeProducts', { title: 'Products', products });
 });
+
+viewsRouter.post("/", (req, res) => {
+  const response = myProducts.addProducts(req.body);
+  
+  res.status(201).send(response);
+});
+
 
 export default viewsRouter;
