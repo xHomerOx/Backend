d768a6c4702a2a0664422d4989443861e97040aa
OTELLO BOZ MARALDO <oboz@agn.gov.ar>
2024-04-07 17:45:38 -0300

    AppJS

--- src/app.js
+++ src/app.js
@@ -1,21 +1,15 @@
 import express from 'express';
-
-//Uso esto para un Layout customizado.
 import exphbs from 'express-handlebars';
 import path from 'path';
 import __dirname from './utils/utils.js';
-
-//Me traigo solo el Router de Carts, por eso lo puse entre llaves.
 import { cartsRouter } from './routes/cartsRoute.js';
 import viewsRouter from './routes/viewsRoute.js';
 import { Server } from 'socket.io';
 
 const app = express();
 
-//Para que solo renderice la vista Home y no el Layout por defecto de Handlebars.
 app.engine('handlebars', exphbs.engine({ defaultLayout: false }));
 
-//Configuro motor y estructura del Proyecto.
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views'));
 
