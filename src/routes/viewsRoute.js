f249c7e0a98fb3dda0922e805aa9a0cbf9443304
OTELLO BOZ MARALDO <oboz@agn.gov.ar>
2024-04-08 11:55:47 -0300

    Cart Postman

--- src/routes/viewsRoute.js
+++ src/routes/viewsRoute.js
@@ -1,6 +1,7 @@
 import express, { Router } from 'express';
 import __dirname from '../utils/utils.js';
 import ProductManager from '../dao/productManagerFS.js';
+import { socketServer } from '../app.js';
 
 const myProduct = new ProductManager('public');
 const viewsRouter = Router();
@@ -14,6 +15,29 @@ viewsRouter.get('/', async (_req, res) => {
 viewsRouter.get('/realtimeproducts', async (_req, res) => {
   const products = await myProduct.getProducts();
   res.render('realTimeProductsView', { title: 'Products', products: products });
+});
+
+viewsRouter.post("/realtimeproducts", async (req, res) => {
+  try {
+    const response = await myProduct.addProducts(req.body);
+    socketServer.emit("productAdded", myProduct.products);
+    console.log()
+    res.status(201).send(response);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+viewsRouter.delete("/realtimeproducts/:id", async (req, res) => {
+  try {
+    const productId = req.params.id;
+    await myProduct.deleteProduct(parseInt(productId));
+    socketServer.emit("productDeleted", productId); 
+
+    res.status(201).send(productId);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 });
 
 export default viewsRouter;
