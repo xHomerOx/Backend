a97f2497bc2af1a92dfdb5981919af13a1d8b120
OTELLO BOZ MARALDO <oboz@agn.gov.ar>
2024-04-15 14:36:24 -0300

    GET ViewRoute

--- src/routes/viewsRoute.js
+++ src/routes/viewsRoute.js
@@ -1,9 +1,6 @@
 import { Router } from 'express';
 import __dirname from '../utils/dirnameUtil.js';
-import ProductManager from '../dao/productManager.js';
-import { socketServer } from '../app.js';
 import productModel from '../dao/models/productModel.js';
-import path from 'path';
 
 const viewsRouter = Router();
 
@@ -111,33 +108,3 @@ viewsRouter.get('/', async (req, res) => {
     });
   }
 });
-
-viewsRouter.get('/', async (_req, res) => {
-  const products = await myProducts.getProducts();
-  res.render('realTimeProductsView', { title: 'Products', products });
-});
-
-viewsRouter.post("/", async (req, res) => {
-  try {
-    const response = await myProducts.addProducts(req.body);
-    socketServer.emit("productAdded", myProducts.products);
-
-    res.status(201).send(response);
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
-});
-
-viewsRouter.delete("/:id", async (req, res) => {
-  try {
-    const productId = req.params.id;
-    await myProduct.deleteProduct(parseInt(productId));
-    socketServer.emit("productDeleted", productId); 
-
-    res.status(201).send(productId);
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
-});
-
-export default viewsRouter;
