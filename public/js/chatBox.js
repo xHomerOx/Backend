e58402ae43fa5f95b3cb3a7db3bc376e350c6273
OTELLO BOZ MARALDO <oboz@agn.gov.ar>
2024-05-10 06:27:29 -0300

    chatbox fix

--- public/js/chatBox.js
+++ public/js/chatBox.js
@@ -16,25 +16,34 @@ Swal.fire({
     inputValidator: (value) => {
         return !value && "Ingrese su usuario/mail";
     }
-}).then(result => {
+}).then(async (result) => {
     user = result.value;
+    await startChat();
 })
 
-chatBox.addEventListener('keyup', e => {
-    if(e.key === "Enter") {
-        if (chatBox.value.trim().length >= 0) {
-            socket.emit("message", {user: user, message: chatBox.value});
-            chatBox.value = '';
+async function startChat() {
+    socket.on("messages", data => {
+        let logs = document.getElementById("message");
+        let messages = '';
+        if (data && data.length > 0) {
+            data.forEach(message => {
+                if (message.user) {
+                    messages += `${message.user}: ${message.message} <br>`;
+                } else {
+                    messages += `<br>`;
+                }
+            })
         }
-    }
-});
-
-socket.on("messages", data => {
-    let logs = document.getElementById("message");
-    let messages = '';
-    data.forEach(message => {
-        messages += `${message.user}: ${message.message} <br>`
+        logs.innerHTML = messages;
+        console.log(messages);
     })
-    logs.innerHTML = messages;
-    console.log(messages);
-})
+
+    chatBox.addEventListener('keyup', e => {
+        if(e.key === "Enter") {
+            if (chatBox.value.trim().length >= 0) {
+                socket.emit("message", {user: user, message: chatBox.value});
+                chatBox.value = '';
+            }
+        }
+    });
+}
