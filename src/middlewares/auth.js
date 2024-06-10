d8e9f98ef05e1393bbd36fa150aed823612ad104
OTELLO BOZ MARALDO <oboz@agn.gov.ar>
2024-06-10 03:35:45 -0300

    Auth Error

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,16 +1,16 @@
 export const auth = function (req, res, next) {
-    const { user } = req.body;
-  
-    if (req.user && req.user.role === 'admin' && req.path !== '/') {
-      req.session.user = user;
-      req.session.admin = true;
-      
-      res.json({
-        status: 'success',
-        user: user,
-        admin: true
-      });
-    } else {
-      return next();
-    }
+  const { user } = req.body;
+
+  if (req.user && req.user.role === 'admin' && req.path !== '/') {
+    req.session.user = user;
+    req.session.admin = true;
+    
+    res.json({
+      status: 'success',
+      user: user,
+      admin: true
+    });
+  } else {
+    return next();
+  }
 }
