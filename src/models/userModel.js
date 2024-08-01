4127d549a1ca4a760572e0c43c97cef7fc6a0817
OTELLO BOZ MARALDO <oboz@agn.gov.ar>
2024-07-31 21:39:50 -0300

    PF Backend

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema({
     },
     role: {
         type: String,
-        enum: ['admin', 'user'],
+        enum: ['admin', 'premium', 'user'],
         default: 'user'
     },
     cart: {
